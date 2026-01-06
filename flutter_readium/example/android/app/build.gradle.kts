450edb4be6acf3dce76ee0d4a4323eaf019a466b
Morten Sjøgren <msn@nota.dk>
2026-01-06 13:59:53 +0100

    chor: update deps to run with flutter 3.38.x

--- flutter_readium/example/android/app/build.gradle.kts
+++ flutter_readium/example/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     // For more information, see: https://flutter.dev/to/review-gradle-config.
     minSdk = 24
     targetSdk = flutter.targetSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
     versionCode = flutter.versionCode
     versionName = flutter.versionName
   }
